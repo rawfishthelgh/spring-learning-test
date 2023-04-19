d079e993647c806e9c0a535c859d728ca824ec75
rawfishthelgh <rg970604@naver.com>
2023-04-19 19:34:46 +0900

    jdbcLearning

--- src/main/java/nextstep/helloworld/jdbc/simpleinsert/SimpleInsertDao.java
+++ src/main/java/nextstep/helloworld/jdbc/simpleinsert/SimpleInsertDao.java
@@ -1,10 +1,14 @@
 package nextstep.helloworld.jdbc.simpleinsert;
 
 import nextstep.helloworld.jdbc.Customer;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository
 public class SimpleInsertDao {
@@ -22,7 +26,11 @@ public class SimpleInsertDao {
      * id를 포함한 Customer 객체를 반환하세요
      */
     public Customer insertWithMap(Customer customer) {
-        return null;
+        Map<String, Object> parameters = new HashMap<>(2);
+        parameters.put("first_name", customer.getFirstName());
+        parameters.put("last_name", customer.getLastName());
+        Long id = insertActor.executeAndReturnKey(parameters).longValue();
+        return new Customer(id, customer.getFirstName(), customer.getLastName());
     }
 
     /**
@@ -31,6 +39,8 @@ public class SimpleInsertDao {
      * id를 포함한 Customer 객체를 반환하세요
      */
     public Customer insertWithBeanPropertySqlParameterSource(Customer customer) {
-        return null;
+        SqlParameterSource parameters = new BeanPropertySqlParameterSource(customer);
+        Long id = insertActor.executeAndReturnKey(parameters).longValue();
+        return new Customer(id, customer.getFirstName(), customer.getLastName());
     }
 }
